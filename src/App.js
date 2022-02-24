2460c92c0fee57d42ccfb92034c597162b6fff03
gdanielcastellanos <gerardo.castellanos@wizeline.com>
2022-02-24 01:30:08 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sample Serverless site for IT Guild Jamay
         </p>
         <a
           className="App-link"
