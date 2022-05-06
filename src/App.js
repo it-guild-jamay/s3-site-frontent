6810b92189a28283300cfa173ea3095d3de59b13
gdanielcastellanos <gerardo.castellanos@wizeline.com>
2022-05-06 16:27:54 -0500

    Testing deploy action

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello IT Guild Jamay!!
+          Hello Community!!
         </p>
         <a
           className="App-link"
